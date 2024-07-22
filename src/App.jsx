392402bbe0e831a6a30f206191f083ed61e63b46
Tariku Mola <97729261+tarikumola@users.noreply.github.com>
2024-07-22 10:04:23 +0300

    add carousel

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
+import CarouselEffect from './components/carousel/CarouselEffect';
+
 
 function App() {
 
@@ -8,6 +10,7 @@ function App() {
   return (
     <>
     <Header />
+    <CarouselEffect />
     </>
   )
 }
